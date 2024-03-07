ddc6262c83d1c2c65d66a132f11ce05bbe5b8b71
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-03-07 15:29:10 +0100

    bg-overlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${alata.className} relative flex min-h-screen flex-col items-center bg-white text-black`}
       >
-        <div className="absolute inset-0 bottom-auto z-0 mx-auto h-[40.625rem] w-full max-w-[90rem] bg-[url('/mobile/image-hero.jpg')] bg-cover bg-center bg-no-repeat md:bg-[url('/desktop/image-hero.jpg')]"></div>
+        <div className="absolute inset-0 bottom-auto z-0 mx-auto h-[40.625rem] w-full max-w-[90rem] bg-[url('/mobile/image-hero.jpg')] bg-cover bg-center bg-no-repeat md:bg-[url('/desktop/image-hero.jpg')]">
+          <div className="absolute inset-0 bg-black/40"></div>
+        </div>
         <Header />
         {children}
         <Footer />
